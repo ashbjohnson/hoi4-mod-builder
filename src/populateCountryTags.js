12f53802425c6a4a2eb9a16729c4273f56ca5d87
johnsnoa <61244971+johnsnoa@users.noreply.github.com>
2024-02-18 13:11:00 -0600

    Added script to populate countries array

---
+++ src/populateCountryTags.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const gameDirectory = require('./gameDirectory.json');
+
+if (!process.argv[2]) {
+    const countriesLoc = Object.fromEntries(fs.readFileSync(gameDirectory.dir + '\\localisation\\english\\countries_l_english.yml').toString().split('\n').filter((line) => /^ \w{3}:0 ".+"/.test(line)).map((line) => {
+        const [tag, ...name] = line.trim().replaceAll(/:0|"/g, '').split(' ');
+        return [tag, name.join(' ')];
+    }));
+    fs.writeFileSync('./countryTags.json', JSON.stringify(countriesLoc));
+
+    console.log('Wrote', Object.entries(countriesLoc).length, 'entries to countryTags.json');
+}
